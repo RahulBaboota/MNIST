0a577622f19cce0bd1bf33106fd293176f3f7b16
RahulBaboota <rahulbaboota08@gmail.com>
2016-11-01 00:23:15 +0530

    Read in MNIST Data

--- Softmax.py
+++ Softmax.py
@@ -3,3 +3,12 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+def main():
+
+	## Reading in the MNIST Data with one_hot encoding set as True for the labels of the images .
+	MNIST = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+	
