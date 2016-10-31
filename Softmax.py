71080d7ceccff2311cf81c3baa4551ca66f59461
RahulBaboota <rahulbaboota08@gmail.com>
2016-11-01 00:21:09 +0530

    Importing necessary libraries from __future__

--- Softmax.py
+++ Softmax.py
@@ -1,1 +1,5 @@
 ''' Implementation of Softmax Regression on the famous MNIST Dataset by following code mentioned in the Tensorflow Tutorial . '''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
