ecc64fea8611a1fd7728866899b58adb1f8469f2
RahulBaboota <rahulbaboota08@gmail.com>
2016-11-02 22:20:08 +0530

    Trained Neural Network

--- Softmax.py
+++ Softmax.py
@@ -8,28 +8,39 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 def main():
 
-	## Reading in the MNIST Data with one_hot encoding set as True for the labels of the images .
-	MNIST = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+    ## Reading in the MNIST Data with one_hot encoding set as True for the labels of the images .
+    MNIST = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
-	## Creating the Model
+    ## Creating the Model
 
-	## Creating a placeholder tensor for the input images . Herein , 'None' refers to any number and 784 = 28*28 which is basically
-	## the dimensions of each image when it is squished into a coloumn/row vector . 
-	x = tf.placeholder(tf.float32, [None,784])
+    ## Creating a placeholder tensor for the input images . Herein , 'None' refers to any number and 784 = 28*28 which is basically
+    ## the dimensions of each image when it is squished into a coloumn/row vector . 
+    x = tf.placeholder(tf.float32, [None,784])
 
-	## Creating the variables 'Weights' and 'Bias' of appropriate dimensions . Since we are eventually going to learn these 
-	## parameters , they are initialised to 0 .
-	W = tf.Variable(tf.zeros([784, 10]))
-  	b = tf.Variable(tf.zeros([10]))
+    ## Creating the variables 'Weights' and 'Bias' of appropriate dimensions . Since we are eventually going to learn these 
+    ## parameters , they are initialised to 0 .
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
 
-  	## We now compute the output of the first Fully-Connected Layer .
-  	y = tf.matmul(x,W) + b
+    ## We now compute the output of the first Fully-Connected Layer .
+    y = tf.matmul(x,W) + b
 
-  	## Creating a placeholder matrix to contain the correct class of the digit image .
-  	y_ = tf.placeholder(tf.float32, [None,10])
+    ## Creating a placeholder matrix to contain the correct class of the digit image .
+    y_ = tf.placeholder(tf.float32, [None,10])
 
-  	## Here , instead of simply optimising the Softmax Loss Function , we are optimising the Cross-Entropy .
-  	Cross_Entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    ## Here , instead of simply optimising the Softmax Loss Function , we are optimising the Cross-Entropy .
+    Cross_Entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 
-  	## We now train our Neural Net to optimise the Cross-Entropy using simple Gradient Descent with a learning rate of 0.5 .
-  	Train_Step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    ## We now train our Neural Net to optimise the Cross-Entropy using simple Gradient Descent with a learning rate of 0.5 .
+    Train_Step = tf.train.GradientDescentOptimizer(0.5).minimize(Cross_Entropy)
+
+    ## Get Session from Tensorflow .
+    Sess = tf.InteractiveSession()
+
+    tf.initialize_all_variables().run()
+
+    ## We run the training step 1000 times wherein each time the Network gets a random batch of 100 data points . 
+    for _ in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
