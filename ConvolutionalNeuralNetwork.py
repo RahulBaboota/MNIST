fb780c70ada0964d18b6f9686f32db7e54e0303a
RahulBaboota <rahulbaboota08@gmail.com>
2016-11-17 17:51:11 +0530

    Input MNIST Data

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -4,3 +4,6 @@ import input_data
 import tensorflow as tf
 
 Sess = tf.InteractiveSession()
+
+## Reading in the MNIST Data with one_hot encoding set as True for the labels of the images .
+MNIST = input_data.read_data_sets("FLAGS.data_dir", one_hot=True)
