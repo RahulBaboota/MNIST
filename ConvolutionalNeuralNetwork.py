92ccca532df7d3d3a7f97a850fa6a5b4f5eaaa4d
RahulBaboota <rahulbaboota08@gmail.com>
2016-11-17 18:40:43 +0530

    Created Weight Initialising Function

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -7,3 +7,17 @@ Sess = tf.InteractiveSession()
 
 ## Reading in the MNIST Data with one_hot encoding set as True for the labels of the images .
 MNIST = input_data.read_data_sets("FLAGS.data_dir", one_hot=True)
+
+## Since we are going to build a Deep Convolutional Neural Network , we may be needing a lot of Weight and Bias Matrices .
+## Therefore , to make our code more modular in architecture , we are going to make two seperate functions for initialising
+## and building the above two matrices .
+
+## Function for creating Weight Matrix .
+## The input to this function is the shape of the weight matrix that we want . It is generally recommended to incorporate small
+## amount of noise while initialising the Weight Matrices to break some symmetry , therefore , we have set the Standard Deviation
+## to 0.1 . Also , the weights are normally distributed as well as truncated i.e. those values which are greater than 2 standard
+## deviations from the mean are dropped . 
+
+def Weight_Variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
