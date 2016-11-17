a44eceed4ceed7c2177d8cd018ff8484eb445e8a
RahulBaboota <rahulbaboota08@gmail.com>
2016-11-17 17:50:00 +0530

    Created Interactive Tensorflow Session

---
+++ ConvolutionalNeuralNetwork.py
@@ -0,0 +1,6 @@
+''' Implementation of a Convolutional Neural Network on the MNIST Data by following code mentioned in the Tensorflow Tutorial . '''
+
+import input_data
+import tensorflow as tf
+
+Sess = tf.InteractiveSession()
